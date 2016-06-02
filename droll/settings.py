2b04dd5122a9943d8456b471c85ee916350bf959
Sergej Alikov <sergej.alikov@gmail.com>
2016-06-02 22:31:40 +0100

    Configure template and static file dirs

--- droll/settings.py
+++ droll/settings.py
@@ -90,7 +90,7 @@ ROOT_URLCONF = 'droll.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,6 +130,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 STATIC_URL = '/static/'
 
