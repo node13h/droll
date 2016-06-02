54893e6db0a27b809403aa5bb481f3a5aa322f26
Sergej Alikov <sergej.alikov@gmail.com>
2016-06-02 22:29:07 +0100

    Add OtpForm

--- access/forms.py
+++ access/forms.py
@@ -1,14 +1,35 @@
-from django.contrib.auth import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 from .models import User
 
 
-class UserCreationForm(forms.UserCreationForm):
-    class Meta(forms.UserCreationForm.Meta):
+class UserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
         model = User
         fields = ("email",)
 
 
-class UserChangeForm(forms.UserChangeForm):
-    class Meta(forms.UserChangeForm.Meta):
+class UserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
         model = User
+
+
+class OtpForm(forms.Form):
+    code = forms.CharField(max_length=6, min_length=6, label=_('code'),
+                           widget=forms.PasswordInput())
+
+    def __init__(self, request, *args, **kwargs):
+        assert request.user.is_authenticated(), (
+            'OtpForm shoud never be used for unauthenticated users')
+
+        self.request = request
+        super().__init__(*args, **kwargs)
+
+    def clean_code(self):
+        user = self.request.user
+        code = self.cleaned_data.get('code')
+
+        if not user.otp_verify(code):
+            raise forms.ValidationError(_('Invalid code'))
--- access/tests/test_forms.py
+++ access/tests/test_forms.py
@@ -1,15 +1,50 @@
+from unittest.mock import MagicMock, patch
+
 from django.test import TestCase
 
-from ..forms import UserCreationForm, UserChangeForm
+from .. import otp
+from .. import forms
+from .factories import UserFactory
 
 
 class UserCreationFormTestCase(TestCase):
     def test_class(self):
-        form = UserCreationForm()
+        form = forms.UserCreationForm()
         self.assertFalse(form.is_valid())
 
 
 class UserChangeFormTestCase(TestCase):
     def test_class(self):
-        form = UserChangeForm()
+        form = forms.UserChangeForm()
+        self.assertFalse(form.is_valid())
+
+
+class OtpFormTestCase(TestCase):
+    def test_class(self):
+        request = MagicMock()
+        form = forms.OtpForm(request)
+        self.assertFalse(form.is_valid())
+
+    @patch.object(otp, 'verify')
+    def test_valid_code(self, m_verify):
+        m_verify.return_value = True
+        request = MagicMock()
+        request.user = UserFactory.build()
+        form = forms.OtpForm(request, {'code': '000000'})
+        self.assertTrue(form.is_valid())
+
+    @patch.object(otp, 'verify')
+    def test_wrong_code(self, m_verify):
+        m_verify.return_value = False
+        request = MagicMock()
+        request.user = UserFactory.build()
+        form = forms.OtpForm(request, {'code': '000000'})
+        self.assertFalse(form.is_valid())
+
+    @patch.object(otp, 'verify')
+    def test_invalid_code(self, m_verify):
+        m_verify.return_value = True
+        request = MagicMock()
+        request.user = UserFactory.build()
+        form = forms.OtpForm(request, {'code': '0'})
         self.assertFalse(form.is_valid())
