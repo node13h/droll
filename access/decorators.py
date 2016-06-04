72de577354e139e238b3be4a46c61bbd006595e7
Sergej Alikov <sergej.alikov@gmail.com>
2016-06-04 23:52:57 +0100

    Add docstring to otp_required

--- access/decorators.py
+++ access/decorators.py
@@ -5,6 +5,20 @@ from django.conf import settings
 
 
 def otp_required(view=None, verify_url=None, redirect_field_name=REDIRECT_FIELD_NAME):
+    """
+    Redirect to the OTP verify URL if two-factor auth is enabled, user is
+    authenticated and OTP was not verified.
+    In other cases - call the original view
+
+    Use to protect the views with OTP (additionally to normal auth)
+
+    :param view: The decorated viev. None to act as normal decorator
+    :param str verify_url: OTP verify URL to redirect to
+    :param str redirect_field_name: URL query argument to use to forward original URL
+
+    :returns: decorator
+    """
+
     if verify_url is None:
         verify_url = settings.OTP_VERIFY_URL
 
