e30e1b7d807407a0f5bdae8a41ca54c850ca36d8
Sergej Alikov <sergej.alikov@gmail.com>
2016-06-12 15:18:46 +0100

    Add otp_verify view

---
+++ access/tests/test_views.py
@@ -0,0 +1,145 @@
+from unittest.mock import MagicMock
+
+# sensitive_post_parameters requires valid HttpRequest
+from django.test import TestCase, RequestFactory
+from django.conf import settings
+
+from .. import views
+
+
+class OtpVerifyTestCase(TestCase):
+    def test_redirect_unauthenticated(self):
+        request = RequestFactory().get('/otp/')
+        request.user = MagicMock()
+        request.user.is_authenticated.return_value = False
+
+        with self.settings(LOGIN_URL='/login/', LOGIN_REDIRECT_URL='/admin/'):
+            response = views.otp_verify(request, redirect_field_name='go')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['Location'], '/login/?go=/admin/')
+
+    def test_redirect_unauthenticated_redirect_in_get(self):
+        request = RequestFactory().get('/otp/', data={'go': '/admin/'})
+        request.user = MagicMock()
+        request.user.is_authenticated.return_value = False
+
+        with self.settings(LOGIN_URL='/login/'):
+            response = views.otp_verify(request, redirect_field_name='go')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['Location'], '/login/?go=/admin/')
+
+    def test_redirect_unauthenticated_redirect_in_post(self):
+        request = RequestFactory().post('/otp/', data={'go': '/admin/'})
+        request._dont_enforce_csrf_checks = True  # Bypass CSRF checks
+        request.user = MagicMock()
+        request.user.is_authenticated.return_value = False
+
+        with self.settings(LOGIN_URL='/login/'):
+            response = views.otp_verify(request, redirect_field_name='go')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['Location'], '/login/?go=/admin/')
+
+    def test_csrf_protect(self):
+        request = RequestFactory().get('/otp/')
+        request.user = MagicMock()
+        request.user.is_authenticated.return_value = True
+        request.user.two_fa_enabled = True
+        response = views.otp_verify(request)
+        csrf_token = request.META['CSRF_COOKIE']
+
+        request = RequestFactory().post(
+            '/otp/', data={'csrfmiddlewaretoken': csrf_token})
+        request.COOKIES[settings.CSRF_COOKIE_NAME] = csrf_token
+        request.user = MagicMock()
+        request.user.is_authenticated.return_value = False
+        request.user.two_fa_enabled = True
+
+        response = views.otp_verify(request)
+        self.assertEqual(response.status_code, 302)
+
+    def test_csrf_protect_reject(self):
+        request = RequestFactory().post('/otp/')
+        request.user = MagicMock()
+        request.user.is_authenticated.return_value = False
+
+        response = views.otp_verify(request)
+        self.assertEqual(response.status_code, 403)
+
+    def test_never_cache(self):
+        request = RequestFactory().get('/otp/')
+        request.user = MagicMock()
+        request.user.is_authenticated.return_value = False
+
+        response = views.otp_verify(request)
+        self.assertIn('Cache-Control', response)
+
+    def test_sensitive_post_parameters(self):
+        request = RequestFactory().get('/otp/')
+        request.user = MagicMock()
+        request.user.is_authenticated.return_value = False
+
+        views.otp_verify(request)
+        self.assertEqual(request.sensitive_post_parameters, '__ALL__')
+
+    def test_verify_success(self):
+        request = RequestFactory().post('/otp/', data={'go': '/admin/',
+                                                       'code': '000000'})
+        request._dont_enforce_csrf_checks = True
+        request.session = {}
+        request.user = MagicMock()
+        request.user.is_authenticated.return_value = True
+        request.user.two_fa_enabled = True
+        request.user.otp_verify.return_value = True
+
+        response = views.otp_verify(request, redirect_field_name='go')
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['Location'], '/admin/')
+
+    def test_verify_fail(self):
+        request = RequestFactory().post('/otp/', data={'go': '/admin/',
+                                                       'code': '000000'})
+        request._dont_enforce_csrf_checks = True
+        request.user = MagicMock()
+        request.user.is_authenticated.return_value = True
+        request.user.two_fa_enabled = True
+        request.user.otp_verify.return_value = False
+
+        response = views.otp_verify(request, redirect_field_name='go')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('form', response.context_data)
+
+    def test_unsafe_redirect(self):
+        request = RequestFactory().post('/otp/', data={'go': 'irc://localhost',
+                                                       'code': '000000'})
+        request._dont_enforce_csrf_checks = True
+        request.session = MagicMock()
+        request.user = MagicMock()
+        request.user.is_authenticated.return_value = True
+        request.user.two_fa_enabled = True
+        request.user.otp_verify.return_value = True
+
+        with self.settings(LOGIN_REDIRECT_URL='/safe/'):
+            response = views.otp_verify(request, redirect_field_name='go')
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['Location'], '/safe/')
+
+    def test_authenticated_get_context(self):
+        request = RequestFactory().get('/otp/', data={'go': '/admin/'})
+
+        request._dont_enforce_csrf_checks = True
+        request.user = MagicMock()
+        request.user.is_authenticated.return_value = True
+        request.user.two_fa_enabled = True
+
+        response = views.otp_verify(request, redirect_field_name='go',
+                                    extra_context={'extra': 'context'})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('form', response.context_data)
+        self.assertIn('go', response.context_data)
+        self.assertIn('site', response.context_data)
+        self.assertIn('site_name', response.context_data)
+        self.assertIn('extra', response.context_data)
--- access/views.py
+++ access/views.py
@@ -1,3 +1,78 @@
-from django.shortcuts import render
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.debug import sensitive_post_parameters
+from django.contrib.sites.shortcuts import get_current_site
+from django.shortcuts import resolve_url
+from django.utils.http import is_safe_url
+from django.conf import settings
+from django.template.response import TemplateResponse
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.http import HttpResponseRedirect
+from django.contrib.auth.views import redirect_to_login
 
-# Create your views here.
+from .forms import OtpForm
+from . import otp
+
+
+@sensitive_post_parameters()
+@csrf_protect
+@never_cache
+def otp_verify(request, template_name='registration/otp.html',
+               login_url=None,
+               redirect_field_name=REDIRECT_FIELD_NAME,
+               otp_form=OtpForm,
+               current_app=None, extra_context=None):
+    """
+    Display OTP form and handle the verification action if user is
+    authenticated. Redirect to the URL specified in
+    redirect_field_name argument otherwise. Resembles
+    login view from django.contrib.auth.views
+
+    :param HttpRequest request: The request object
+    :param str vtemplate_name: File to use as a template for the view
+    :param str redirect_field_name: URL query argument containing URL
+        to redirect to
+    :param Form form: Form class to use in the view
+    :param str current_app: Current app name
+    :param dict extra_context: Extra context dict to pass to the template
+
+    :returns: HttpResponse
+    """
+
+    redirect_to = request.POST.get(redirect_field_name,
+                                   request.GET.get(redirect_field_name, ''))
+
+    # Ensure the user-originating redirection url is safe.
+    if not is_safe_url(url=redirect_to, host=request.get_host()):
+        redirect_to = resolve_url(settings.LOGIN_REDIRECT_URL)
+
+    resolved_login_url = resolve_url(login_url or settings.LOGIN_URL)
+
+    if not request.user.is_authenticated():
+        return redirect_to_login(redirect_to, login_url=resolved_login_url,
+                                 redirect_field_name=redirect_field_name)
+
+    if request.method == 'POST':
+        form = otp_form(request, request.POST)
+
+        if form.is_valid():
+            otp.set_verified(request)
+            return HttpResponseRedirect(redirect_to)
+    else:
+        form = otp_form(request)
+
+    current_site = get_current_site(request)
+
+    context = {
+        'form': form,
+        redirect_field_name: redirect_to,
+        'site': current_site,
+        'site_name': current_site.name,
+    }
+    if extra_context is not None:
+        context.update(extra_context)
+
+    if current_app is not None:
+        request.current_app = current_app
+
+    return TemplateResponse(request, template_name, context)
