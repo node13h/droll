1fab15d511f2b4451ece930a5a461ccc09ea15f9
Sergej Alikov <sergej.alikov@gmail.com>
2016-07-08 00:11:02 +0100

    Add html5boilerplate-based basic template

--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -3,6 +3,7 @@ from unittest.mock import MagicMock
 from django.test import TestCase, RequestFactory
 
 from .. import views
+from .factories import PostFactory
 
 
 class RollViewTestCase(TestCase):
@@ -16,3 +17,27 @@ class RollViewTestCase(TestCase):
 
         response = self.view(request)
         self.assertContains(response, '<html')
+
+    def test_template(self):
+        request = RequestFactory().get('/')
+        request.user = MagicMock()
+        request.user.is_authenticated.return_value = False
+
+        PostFactory(body='POST-BODY-MARKER', public=True)
+
+        response = self.view(request)
+        self.assertContains(response, 'POST-BODY-MARKER')
+
+    def test_public(self):
+        request = RequestFactory().get('/')
+        request.user = MagicMock()
+        request.user.is_authenticated.return_value = False
+
+        PostFactory(body='POST1-BODY-MARKER', public=True)
+        PostFactory(body='POST2-BODY-MARKER')
+
+        response = self.view(request)
+
+        posts = response.context_data['object_list']
+        self.assertTrue(posts.filter(body='POST1-BODY-MARKER').exists())
+        self.assertFalse(posts.filter(body='POST2-BODY-MARKER').exists())
