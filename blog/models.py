fb59b895de6e347413cc48714404b190379e74fa
Sergej Alikov <sergej.alikov@gmail.com>
2016-07-17 11:30:43 +0100

    Implement Post.objects.roll()

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,39 @@
 from django.db import models
+from django.db.models import Q
 from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
 from django.conf import settings
+
+
+class PostManager(models.Manager):
+    def roll(self, user, *, limit=None, start=None):
+        """
+        Return personalized blog roll
+
+        :param User user: user to prepare personalized roll for
+        :param int limit: maximum number of object to return
+        :param datetime start: minimum timestamp to return
+
+        :rtype: QuerySet
+        :returns: filtered QuerySet
+        """
+
+        qs = self.get_queryset()
+
+        if start is not None:
+            qs = qs.filter(timestamp__gte=start)
+
+        f = Q(public=True)
+
+        if user.is_authenticated():
+            f |= Q(user=user)
+
+        qs = qs.filter(f).order_by('-timestamp')
+
+        if limit is not None:
+            qs = qs[:limit]
+
+        return qs
 
 
 class Post(models.Model):
@@ -11,3 +43,5 @@ class Post(models.Model):
     timestamp = models.DateTimeField(_('timestamp'), default=timezone.now, db_index=True)
     title = models.CharField(_('title'), max_length=140)
     body = models.TextField(_('body'))
+
+    objects = PostManager()
--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -1,6 +1,12 @@
+from unittest.mock import MagicMock
+from datetime import datetime
+
 from django.test import TestCase
+from django.utils.timezone import utc
 
 from .factories import PostFactory
+from ..models import Post
+from access.tests.factories import UserFactory
 
 
 class PostTestCase(TestCase):
@@ -8,3 +14,65 @@ class PostTestCase(TestCase):
         post = PostFactory()
 
         self.assertTrue(post.pk)
+
+    def test_manager_roll_non_public_own_only(self):
+        user1 = UserFactory()
+        user2 = UserFactory()
+
+        post1 = PostFactory(user=user1, public=True)
+        post2 = PostFactory(user=user1, public=False)
+        post3 = PostFactory(user=user2, public=True)
+        post4 = PostFactory(user=user2, public=False)
+
+        roll = Post.objects.roll(user1)
+
+        self.assertIn(post1, roll)
+        self.assertIn(post2, roll)
+        self.assertIn(post3, roll)
+        self.assertNotIn(post4, roll)
+
+    def test_manager_roll_unauthenticated_public_only(self):
+        user1 = MagicMock()
+        user1.is_authenticated.return_value = False
+        user2 = UserFactory()
+
+        post1 = PostFactory(user=user2, public=True)
+        post2 = PostFactory(user=user2, public=False)
+
+        roll = Post.objects.roll(user1)
+
+        self.assertIn(post1, roll)
+        self.assertNotIn(post2, roll)
+
+    def test_manager_roll_default_order_newest_to_oldest(self):
+        user = UserFactory()
+
+        post1 = PostFactory(user=user, timestamp=datetime(1980, 5, 3, tzinfo=utc))
+        post2 = PostFactory(user=user, timestamp=datetime(1980, 5, 2, tzinfo=utc))
+        post3 = PostFactory(user=user, timestamp=datetime(1980, 5, 6, tzinfo=utc))
+
+        roll_list = list(Post.objects.roll(user))
+
+        self.assertEqual(roll_list, [post3, post1, post2])
+
+    def test_manager_roll_limit(self):
+        user = UserFactory()
+
+        post1 = PostFactory(user=user, timestamp=datetime(1980, 5, 3, tzinfo=utc))
+        post2 = PostFactory(user=user, timestamp=datetime(1980, 5, 2, tzinfo=utc))
+
+        roll = Post.objects.roll(user, limit=1)
+
+        self.assertIn(post1, roll)
+        self.assertNotIn(post2, roll)
+
+    def test_manager_roll_start(self):
+        user = UserFactory()
+
+        post1 = PostFactory(user=user, timestamp=datetime(1980, 5, 3, tzinfo=utc))
+        post2 = PostFactory(user=user, timestamp=datetime(1980, 5, 2, tzinfo=utc))
+
+        roll = Post.objects.roll(user, start=datetime(1980, 5, 3, tzinfo=utc))
+
+        self.assertIn(post1, roll)
+        self.assertNotIn(post2, roll)
